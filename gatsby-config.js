0fed6b48e87277f52a63f124562866bc257bc61c
Daniel Gading <dgading@gmail.com>
2019-12-13 14:24:23 -0500

    Add Gatsby, airbnb linting, and gh-pages

---
+++ gatsby-config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  pathPrefix: '/FE-knowledgebase',
+  siteMetadata: {
+    title: 'FE-knowledgebase',
+    description: 'A knowledge base for the frontend team at CivicActions.',
+    author: '@civicactions',
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: `${__dirname}/src/images`,
+      },
+    },
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'gatsby-starter-default',
+        short_name: 'starter',
+        start_url: '/',
+        background_color: '#663399',
+        theme_color: '#663399',
+        display: 'minimal-ui',
+        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+      },
+    },
+    'gatsby-plugin-eslint',
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // 'gatsby-plugin-offline',
+  ],
+};
