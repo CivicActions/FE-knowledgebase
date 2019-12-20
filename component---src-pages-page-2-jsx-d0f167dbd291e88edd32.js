b58d584467468f1df2d78e306932f2a96591c550
Daniel Gading <dgading@gmail.com>
2019-12-20 10:08:23 -0500

    Updates

---
+++ component---src-pages-page-2-jsx-d0f167dbd291e88edd32.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{caWp:function(e,t,a){"use strict";a.r(t);var n=a("q1tI"),o=a.n(n),c=a("Wbzz"),l=a("L12J"),r=a("dmsj");t.default=function(){return o.a.createElement(l.a,null,o.a.createElement(r.a,{title:"Page two"}),o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2"),o.a.createElement(c.Link,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-jsx-d0f167dbd291e88edd32.js.map
