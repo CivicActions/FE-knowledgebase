27976a6c55c1a11b593f16a5fe7c2798d6ae0050
Iris Ibekwe <iris.ibekwe@civicactions.com>
2020-02-02 21:11:45 -0500

    Host google fonts locally.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,9 +2,10 @@
  * Implement Gatsby's Browser APIs in this file.
  *
  * See: https://www.gatsbyjs.org/docs/browser-apis/
- * We load our styles and typefaces up front, so that we do not get a FOUC when SSR is rendered but not JS.
+ * We load our styles and typefaces up front,
+ * so that we do not get a FOUC when SSR is rendered but not JS.
  */
 import './src/sass/styles.scss';
 
-require(`typeface-lustria`);
-require(`typeface-lato`);
+require('typeface-lustria');
+require('typeface-lato');
