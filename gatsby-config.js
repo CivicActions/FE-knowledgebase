62544203e75a3e597ef5cbd2516b9720e71fb09f
Iris Ibekwe <iris.ibekwe@civicactions.com>
2020-02-05 22:59:48 -0500

    Add the Strapi source plugin.

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,6 +19,16 @@ module.exports = {
     'gatsby-plugin-sharp',
     'gatsby-plugin-sass',
     {
+      resolve: 'gatsby-source-strapi',
+      options: {
+        apiURL: 'http://localhost:1337',
+        contentTypes: [
+          'recipe',
+          'user'
+        ]
+      }
+    },
+    {
       resolve: 'gatsby-plugin-manifest',
       options: {
         name: 'gatsby-starter-default',
