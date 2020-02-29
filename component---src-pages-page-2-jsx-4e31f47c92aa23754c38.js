e885677c4239942e1c2070e8813312d75d337d98
Iris Ibekwe <iris.ibekwe@civicactions.com>
2020-02-28 20:16:55 -0500

    Updates

--- component---src-pages-page-2-jsx-4e31f47c92aa23754c38.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{caWp:function(e,t,a){"use strict";a.r(t);var n=a("q1tI"),o=a.n(n),c=a("Wbzz"),l=a("L12J"),r=a("dmsj");t.default=function(){return o.a.createElement(l.a,null,o.a.createElement(r.a,{title:"Page two"}),o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2"),o.a.createElement(c.Link,{to:"/"},"Go back to the homepage"))}}}]);
-//# sourceMappingURL=component---src-pages-page-2-jsx-4e31f47c92aa23754c38.js.map
--- styles-eeedb0c12f2484c948ee.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{"6MIl":function(n,o,p){},"8ypT":function(n,o,p){}}]);
-//# sourceMappingURL=styles-eeedb0c12f2484c948ee.js.map
